0d433087987d183ae908527d951e38779d27a0c9
mugomes <murilogomes5792@gmail.com>
2024-10-11 18:57:59 -0300

    Update buttons

--- src/js/script.js
+++ src/js/script.js
@@ -9,14 +9,24 @@ buttons.forEach(button => {
     button.addEventListener('click', () => {
         const nome = button.getAttribute('id');
  
-        const oculta = document.querySelectorAll('.cartao-mgrobot');
-        oculta.forEach(div => {
+        const oculta1 = document.querySelectorAll('.mgrobot');
+        oculta1.forEach(div => {
+            div.classList.remove('ativo');
+        });
+
+        const oculta2 = document.querySelectorAll('.cartao-mgrobot');
+        oculta2.forEach(div => {
             div.classList.remove('aberto');
         });
 
-        const mostra = document.getElementById(`cartao-${nome}`);
-        if (mostra) {
-            mostra.classList.add('aberto');
+        const mostra1 = document.getElementById(nome);
+        if (mostra1) {
+            mostra1.classList.add('ativo');
+        }
+
+        const mostra2 = document.getElementById(`cartao-${nome}`);
+        if (mostra2) {
+            mostra2.classList.add('aberto');
         }
     });
 });
